63f171cb37303782de019e145080c732e291c5ef
Hebert <hebertphp@gmail.com>
2022-08-29 11:36:32 -0300

    PFE_2a_M 29/08/22

---
+++ aula2/script2_01.js
@@ -0,0 +1,11 @@
+//alert("Mensagem");//caixa de alerta
+document.write("<h2>Outra mensagem!</h2>");//escreve o que estiver entre paren na página
+//document.write("<br>");
+document.write(10 * 3);
+document.write("---");
+console.log("Onde aparece?");
+/*
+posso 
+escrever
+a vontade
+*/
