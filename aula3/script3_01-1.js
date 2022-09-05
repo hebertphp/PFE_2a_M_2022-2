f38796f71cae0b31e8afbec9a73e8b2d3258c78b
Hebert <hebertphp@gmail.com>
2022-09-05 11:36:33 -0300

    PFE_2a_M 05-09-22

---
+++ aula3/script3_01-1.js
@@ -0,0 +1,22 @@
+let oct = 010;
+
+let hr;
+hr = "<hr>";
+let br = "<br>";
+br = "<hr>";
+let bin = 0b11, hexa = 0x10;
+let dia, nota, cep;
+let x, y, z;
+dia = 05;
+nota = 9.7;
+cep = "03312123";
+document.write ("dia: ",dia,"<br>");
+document.write ("nota: ",nota,"<br>");
+document.write(oct,"<br>",bin,"<br>",hexa);
+document.write("<br>");
+document.write("cep: ",cep,br);
+/*
+decimal 0123456789 10 11 12 13
+octal   01234567  10 11
+bin 0 1 10 11 
+*/
