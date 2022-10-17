62c1439fafeccdcb4597610ad524f18f9e20dca9
Hebert <hebertphp@gmail.com>
2022-10-17 10:52:00 -0300

    17/10/22

---
+++ aula5/script5_desafio1.js
@@ -0,0 +1,8 @@
+/*
+Solicitar a entrada de duas notas (av1 e av2)
+Calcular a média entre av1 e av2
+Mostrar uma das mensagens
+Qual a condição para mostrar uma das mensagens abaixo?
+"Aprovado"
+"Reprovado"
+*/
