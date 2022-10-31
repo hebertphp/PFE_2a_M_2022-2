4856a39b3400d88e18cc18ac8d87c2ee326ad531
Hebert <hebertphp@gmail.com>
2022-10-31 11:27:23 -0300

    31/10/22

--- aula5/script5_07-2.js
+++ aula5/script5_07-2.js
@@ -2,8 +2,19 @@
 desafio:
 
 converter o código script5_07.js
-de switch-case para if else if else
+de switch-case para if, else if, else
 */
 let destino,msg;
 const saida=document.querySelector(".saida");
 destino=prompt("Digite seu destino: ");
+destino=destino.toUpperCase();//transforma o texto em letras maiúsculas
+
+if(destino == "SÃO PAULO"){
+    saida.innerHTML=`Seja bem vindo a cidade que nunca para!`;
+ }
+ else if (destino == "NEW YORK"){
+     saida.innerHTML=`Seja bem vindo a Big Apple!`;
+ }
+ else{
+     saida.innerHTML=`Não encontramos seu destino`;
+ }
---
+++ aula5/script5_08-1.js
@@ -0,0 +1,59 @@
+let i;
+const saida=document.querySelector(".saida");
+i=0; //valor inicial
+while (i <= 5) {//condição
+    saida.innerHTML+=i + " ";
+    i++;//contador
+}
+saida.innerHTML+="<hr>";
+/*
+Saída: 0 1 2 3 4 5
+
+i   i<=5 saída   contador:i++
+0   T   0       1
+1   T   1       2
+2   T   2       3
+3   T   3       4
+4   T   4       5
+5   T   5       6
+6   F
+*/
+/*
+Desafio1: Mostrar a saída: 9 10 11 12
+*/
+i=9; //valor inicial
+
+while (i < 13) {//condição i<13 ou i<=12
+    saida.innerHTML+=i + " ";
+    i++;//contador
+}
+saida.innerHTML+="<hr>";
+/*
+Desafio2: Mostrar a saída: 3 2 1 0
+*/
+i=3;
+while(i>=0){
+    saida.innerHTML+=i + " ";
+    i--;//contador
+}
+saida.innerHTML+="<hr>";
+/*
+Desafio3: Mostrar a saída: 0 2 4 6 8 10
+*/
+i=0;
+while(i<=10){
+    saida.innerHTML+=i + " ";
+    //i++;//contador //i=i+1  //i+=1
+    i+=2;
+}
+saida.innerHTML+="<hr>";
+/*
+Desafio4: Mostrar a saída: 0 4 8 
+*/
+i=0;
+while(i<=10){
+    saida.innerHTML+=i + " ";
+    //i++;//contador //i=i+1  //i+=1
+    i+=4;
+}
+saida.innerHTML+="<hr>";
