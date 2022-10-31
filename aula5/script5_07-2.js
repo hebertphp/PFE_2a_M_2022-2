d45ab1dddfd09af3df9ee6ba1353b9db9c8883ce
Hebert <hebertphp@gmail.com>
2022-10-31 10:09:26 -0300

    31/10/22

---
+++ aula5/script5_07-2.js
@@ -0,0 +1,9 @@
+/*
+desafio:
+
+converter o código script5_07.js
+de switch-case para if else if else
+*/
+let destino,msg;
+const saida=document.querySelector(".saida");
+destino=prompt("Digite seu destino: ");
