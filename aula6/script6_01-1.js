dc52aef52aaf3421fa6feb21ed4e5b47608559ac
Hebert <hebertphp@gmail.com>
2022-11-21 10:35:24 -0300

    21/11/22

--- aula6/script6_01-1.js
+++ aula6/script6_01-1.js
@@ -1,5 +1,13 @@
+//           0     1    2    3
 const uf = ["SP","RJ","MG","ES"];
+//                0    1   2
 const valores = ["SP",7.5,2021];
 const saida=document.querySelector('.saida');
 
-saida.innerHTML=`${uf} <br> ${valores}`;
+saida.innerHTML=`${uf[1]} <br> ${valores[1]}`;
+/*
+imprimiu o nome da var array, mostra todos os valores
+uf[2] valores[2]
+MG
+2021
+*/
---
+++ aula6/script6_01-2.js
@@ -0,0 +1,6 @@
+/*
+Criar uma var do tipo array com os seguintes valores - lista:
+Arroz,Feijão,Carne,Leite
+Mostrar as saídas:
+Todos os valores e mostrar um de cada vez.
+*/
