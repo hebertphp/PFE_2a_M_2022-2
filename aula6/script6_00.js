201f68815bfb0aabafa50b0eb426460881901f91
Hebert <hebertphp@gmail.com>
2022-11-21 10:17:34 -0300

    21/11/22

---
+++ aula6/script6_00.js
@@ -0,0 +1,8 @@
+const saida=document.querySelector('.saida');
+let i,j;//declaração
+i=10;// var i recebe o valor 10, o valor 10 foi a atribuido para a var i
+i=20;//i vale 20
+i=30;//i vale 30
+i=2;//
+j=[10,20,30,2,"teste"];//array
+saida.innerHTML=j;
