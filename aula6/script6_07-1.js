631c724c67a42dac0e78b45a1e2dc941aed3c494
Hebert <hebertphp@gmail.com>
2022-12-05 09:32:39 -0300

    05/12/22

--- aula6/script6_07-1.js
+++ aula6/script6_07-1.js
@@ -1,11 +1,11 @@
 const saida=document.querySelector('.saida');
-const estudante=['Astrogildo','Belarmina','Pafuncia','Alberto'];
-
+const estudante=['Astrogildo','Belarmina','Pafuncia','Alberto','Outro nome'];
+let n="oi";
 //for(let var_simples of var_tipo_array)
 //for of, serve para pegar todos os valores do array, onde
 // o foco será nos valores.
 
-for (let n of estudante){
+for (n of estudante){
     saida.innerHTML+=`${n}<br>`;
 }
 /*
