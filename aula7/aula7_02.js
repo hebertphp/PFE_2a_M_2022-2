4535da89d9e6ab4a39f3da42d5e3413827dc69f1
Hebert <hebertphp@gmail.com>
2022-12-12 10:54:20 -0300

    12/12/22

--- aula7/aula7_02.js
+++ aula7/aula7_02.js
@@ -15,15 +15,18 @@ const saida=document.querySelector(".saida");
 const nome=document.querySelector("#nome");
 const av1=document.querySelector("#av1");
 const av2=document.querySelector("#av2");
+const btn1=document.querySelector(".btn1");
+const btn2=document.querySelector(".btn2");
 
+btn1.onclick=calcmedia;
 
 function boas_vindas(){//construção da função
     saida.innerHTML+="Seja bem vindo!<br>";
 }
 function calcmedia(){
-/* como calcular a media entre av1 e av2? */
+    let media=(parseFloat(av1.value)+parseFloat(av2.value))/2;
     saida.innerHTML=`Nome: ${nome.value}<br>`;
     saida.innerHTML+=`AV1:  ${av1.value}<br>`;
     saida.innerHTML+=`AV2:  ${av2.value}<br>`;
-
+    saida.innerHTML+=`Média:  ${media}<br>`;
 }
