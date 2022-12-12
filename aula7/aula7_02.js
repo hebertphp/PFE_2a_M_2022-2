f121b4f579dfa2439b8d6811dd01554a3c46ffbf
Hebert <hebertphp@gmail.com>
2022-12-12 10:24:23 -0300

    12/12/22

--- aula7/aula7_02.js
+++ aula7/aula7_02.js
@@ -12,6 +12,18 @@ Nota AV2
 Média final
 */
 const saida=document.querySelector(".saida");
+const nome=document.querySelector("#nome");
+const av1=document.querySelector("#av1");
+const av2=document.querySelector("#av2");
+
+
 function boas_vindas(){//construção da função
     saida.innerHTML+="Seja bem vindo!<br>";
+}
+function calcmedia(){
+/* como calcular a media entre av1 e av2? */
+    saida.innerHTML=`Nome: ${nome.value}<br>`;
+    saida.innerHTML+=`AV1:  ${av1.value}<br>`;
+    saida.innerHTML+=`AV2:  ${av2.value}<br>`;
+
 }
